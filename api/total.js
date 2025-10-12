8ca8c0d35a0fc86a779e6d64842ed56b0586846a
mrtlhn <39784115+mrtlhn@users.noreply.github.com>
2025-10-12 12:23:09 +0300

    Update total.js

--- api/total.js
+++ api/total.js
@@ -1,1 +1,8 @@
+// /api/total.js
+export default function handler(req, res) {
+  // Total supply is fixed at 1,000,000,000 XALRA
+  res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate=604800");
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send("1000000000");
+}
 
